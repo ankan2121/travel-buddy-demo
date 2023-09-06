acf1ffd36a46d95ba714ba1dd56109fd1e104b7d
Ankan Nayak <A12NAYAK@gmail.com>
2023-09-06 15:28:42 +0530

    for docker

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template
 
 app = Flask('__name__')
 
+
+
 @app.route('/')
 def home():
-    return 'Hello world!!'
+    name = "my name is Slim Shady"
+    return f'Hi {name}'
 
-app.run(debug=True)
+app.run(host='0.0.0.0',port=5000, debug=True)
